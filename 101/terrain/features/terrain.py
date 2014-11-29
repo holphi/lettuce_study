f41e63375c0715b74a7d0b0674ae0792df2eca33
holphi <holphi@outlook.com>
2014-11-29 10:34:35 +0800

    - Delete terrain.py

--- 101/terrain/features/terrain.py
+++
@@ -1,9 +0,0 @@
-#Terrain will be executed before lettuce tests
-
-'''
-
-By convention lettuce tries do load a file called terrain.py located at the current directory.
-
-'''
-
-print 'This is terrain.py!'
